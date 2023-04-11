58ea62b26af969196ee2d36efc626a0d90dd395a
Kunj <viradia.kunj10@gmail.com>
2023-04-11 12:41:03 +0530

    change server setting for deployment

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
@@ -16,11 +18,14 @@ connectDB();
 
 //rest object
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -28,9 +33,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__filename, './client/build/index.html'))
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
