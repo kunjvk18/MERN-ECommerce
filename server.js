bf2d6437c32956c98caf78d201bb6693f9fd5f2e
Kunj <viradia.kunj10@gmail.com>
2023-04-11 13:56:08 +0530

    change server setting for deployment

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDB();
 //rest object
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 //middelwares
 app.use(cors());
